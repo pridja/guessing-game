9f2fd765280b0d7677ed1c1ef0cdba8924a15d47
prijda <pridja@gmail.com>
2015-11-04 18:34:08 -0800

    first commit

---
+++ game.js
@@ -0,0 +1,51 @@
+    var alert("Hi Tom");
+
+    var userName = "Tom";
+    var guess1 = prompt("Has Priya ever lived overseas?");
+    if (guess1 == "yes" || guess1 == "y") {
+      alert("Great work, Tom.  Priya lived in Great Britain!");
+    }
+    else if (guess1 == "no" || guess1 == "n") {
+      alert("That is incorrect, she once lived across the pond!");
+    }
+    else {
+      alert("Here is a hint: She used to have an accent!");
+    }
+
+    var guess2 = prompt("Does Priya have dogs as pets?");
+    if (guess2 == "yes" || guess2 == "y") {
+      alert("Unfortunately not, she doesn't have the time!");
+    }
+    else if (guess2 == "no" || guess2 == "n") {
+      alert("That is right, she is a cat lady through and through!");
+    }
+      else {
+        alert("She is an animal lover!");
+    }
+
+    var guess3 = prompt("Is Priya's favorite color green?");
+    if (guess3 == "yes" || guess3 == "y") {
+      alert("Although that is a wonderful color, green is not her favorite.");
+    }
+    else if (guess3 == "no" || guess3 == "n") {
+      alert("Right you are!  Priya is partial to purple!");
+    }
+    else {
+      alert("All colors are special and magnificent!")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
