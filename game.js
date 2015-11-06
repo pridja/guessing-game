998555f3890d81c6dfa62c9fdb0ceb5cad2f14aa
prijda <pridja@gmail.com>
2015-11-05 18:42:43 -0800

    linked JS file/added 6th question

--- game.js
+++ game.js
@@ -2,37 +2,69 @@
 
     var userName = "Tom";
     var guess1 = prompt("Has Priya ever lived overseas?");
-    if (guess1 == "yes" || guess1 == "y") {
-      alert("Great work, Tom.  Priya lived in Great Britain!");
-    }
-    else if (guess1 == "no" || guess1 == "n") {
-      alert("That is incorrect, she once lived across the pond!");
-    }
-    else {
-      alert("Here is a hint: She used to have an accent!");
-    }
-
+      if (guess1 == "yes" || guess1 == "y") {
+        alert("Great work, Tom.  Priya lived in Great Britain!");
+        }
+        else if (guess1 == "no" || guess1 == "n") {
+        alert("That is incorrect, she once lived across the pond!");
+        }
+        else {
+        alert("Here is a hint: She used to have an accent!");
+        }
     var guess2 = prompt("Does Priya have dogs as pets?");
-    if (guess2 == "yes" || guess2 == "y") {
-      alert("Unfortunately not, she doesn't have the time!");
-    }
-    else if (guess2 == "no" || guess2 == "n") {
-      alert("That is right, she is a cat lady through and through!");
-    }
-      else {
+      if (guess2 == "yes" || guess2 == "y") {
+        alert("Unfortunately not, she doesn't have the time!");
+        }
+        else if (guess2 == "no" || guess2 == "n") {
+        alert("That is right, she is a cat lady through and through!");
+        }
+        else {
         alert("She is an animal lover!");
-    }
-
+        }
     var guess3 = prompt("Is Priya's favorite color green?");
-    if (guess3 == "yes" || guess3 == "y") {
-      alert("Although that is a wonderful color, green is not her favorite.");
-    }
-    else if (guess3 == "no" || guess3 == "n") {
-      alert("Right you are!  Priya is partial to purple!");
-    }
-    else {
-      alert("All colors are special and magnificent!")
-    }
+      if (guess3 == "yes" || guess3 == "y") {
+         alert("Although that is a wonderful color, green is not her favorite.");
+        }
+        else if (guess3 == "no" || guess3 == "n") {
+        alert("Right you are!  Priya is partial to purple!");
+        }
+        else {
+        alert("All colors are special and magnificent!");
+        }
+    var guess4 = prompt("Is Priya left-handed?");
+      if (guess4 == "no" || guess4 == "n") {
+        alert("You might be the smartest person in the room! Priya is right-handed!");
+        }
+        else if (guess4 == "yes" || guess4 == "y") {
+          alert("You are one pathetic loser! Guess again!");
+        }
+        else {
+          alert("Cannot compute response, 'yes' or 'no' will do.");
+        }
+    var guess5 = prompt("Does Priya love to bake?");
+      if (guess5 == "no" || guess5 == "n") {
+        alert("You couldn't be more wrong!");
+        }
+        else if (guess5 == "yes" || "y") {
+          alert("Right again! Cookies, brownies, you name it - she'll bake it!");
+        }
+        else {
+          alert("Just call her Cookie Monster!");
+        }
+
+        var guess = Number(prompt("How old is Priya?"));
+        if (guess > 32) {
+            alert("How dare you.");
+        }
+        else if (guess < 32) {
+            alert("Bless your heart! Young at heart, yes but guess a little higher.");
+        }
+        else (guess == 32) {
+            alert("Right on the money! Great job!");
+        }
+
+
+
 
 
 
